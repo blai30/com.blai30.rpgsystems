51ccb0735ee3b7cda2a1a437209a0f7298196932
Brian Lai <brianlai30@outlook.com>
2020-04-28 02:49:56 -0700

    Added abbreviation for stat type

--- Runtime/Stats/IStatType.cs
+++ Runtime/Stats/IStatType.cs
@@ -3,6 +3,7 @@ namespace blai30.RPGSystems.Stats
     public interface IStatType
     {
         string Name { get; }
+        string Abbreviation { get; }
         float DefaultValue { get; }
     }
 }
--- Runtime/Stats/StatType.cs
+++ Runtime/Stats/StatType.cs
@@ -8,9 +8,11 @@ namespace blai30.RPGSystems.Stats
     public class StatType : ScriptableObject, IStatType
     {
         [SerializeField] private new string name = "New Stat Type Name";
+        [SerializeField] private string abbreviation = "ABC";
         [SerializeField] private float defaultValue = 0f;
 
         public string Name => name;
+        public string Abbreviation => abbreviation;
         public float DefaultValue => defaultValue;
     }
 }
