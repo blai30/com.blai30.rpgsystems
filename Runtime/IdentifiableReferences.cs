b31992fd992b8f025774717bc0323ca093287c31
Brian Lai <brianlai30@outlook.com>
2020-05-25 18:17:59 -0700

    Added IdentifiableReferences and made it generic. ItemDataReferences inherits from it

---
+++ Runtime/IdentifiableReferences.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+using UnityEditor;
+using UnityEngine;
+
+namespace blai30.RPGSystems
+{
+    public abstract class IdentifiableReferences<T> : ScriptableObject where T : ScriptableObject, IIdentifiable
+    {
+#if ODIN_INSPECTOR
+        [FolderPath]
+#endif
+        [SerializeField]
+        private string[] foldersToSearchIn = null;
+
+        [SerializeField]
+        private List<T> references = null;
+
+        [SerializeField]
+        private Dictionary<string, T> stringObjMap = null;
+
+        private bool IsInitialized => stringObjMap != null;
+
+        public T GetById(string itemId)
+        {
+            EnsureInitialized();
+            return stringObjMap[itemId];
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            LoadReferences();
+        }
+
+        public void LoadReferences()
+        {
+            references = FindAssetsByType<T>(foldersToSearchIn);
+            EnsureInitialized();
+        }
+
+        public void ClearReferences()
+        {
+            references = new List<T>();
+            stringObjMap = new Dictionary<string, T>();
+        }
+
+        private static List<TSource> FindAssetsByType<TSource>(params string[] folders) where TSource : ScriptableObject, IIdentifiable
+        {
+            string type = typeof(TSource).Name;
+
+            // Find asset guids with the type
+            if (folders == null || folders.Length <= 0)
+            {
+                return null;
+            }
+
+            var guids = AssetDatabase.FindAssets("t:" + type, folders);
+
+            // Convert asset guids to asset paths
+            List<TSource> assets = new List<TSource>();
+            foreach (var guid in guids)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                assets.Add(AssetDatabase.LoadAssetAtPath<TSource>(assetPath));
+            }
+
+            return assets;
+        }
+#endif
+
+        private void EnsureInitialized()
+        {
+            if (IsInitialized)
+            {
+                return;
+            }
+
+            stringObjMap = new Dictionary<string, T>(references.ToDictionary(item => item.Id));
+        }
+    }
+}
--- Runtime/Inventory/ItemDataReferences.cs
+++ Runtime/Inventory/ItemDataReferences.cs
@@ -1,87 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-#if ODIN_INSPECTOR
-using Sirenix.OdinInspector;
-#endif
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace blai30.RPGSystems.Inventory
 {
     [CreateAssetMenu]
-    public class ItemDataReferences : ScriptableObject
+    public class ItemDataReferences : IdentifiableReferences<ItemData>
     {
-#if ODIN_INSPECTOR
-        [FolderPath]
-#endif
-        [SerializeField]
-        private string[] foldersToSearchIn;
-
-        [SerializeField]
-        private List<ItemData> references;
-
-        [SerializeField]
-        private Dictionary<string, ItemData> stringObjMap;
-
-        public bool IsInitialized => stringObjMap != null;
-
-        public ItemData GetById(string itemId)
-        {
-            EnsureInitialized();
-            return stringObjMap[itemId];
-        }
-
-#if UNITY_EDITOR
-        [ContextMenu("Load References")]
-        public void LoadReferences()
-        {
-            references = FindAssetsByType<ItemData>(foldersToSearchIn);
-            EnsureInitialized();
-        }
-
-        [ContextMenu("Clear References")]
-        public void ClearReferences()
-        {
-            references = new List<ItemData>();
-            stringObjMap = new Dictionary<string, ItemData>();
-        }
-
-        private static List<T> FindAssetsByType<T>(params string[] folders) where T : Object
-        {
-            string type = typeof(T).Name;
-
-            // Find asset guids with the type
-            string[] guids;
-            if (folders == null || folders.Length <= 0)
-            {
-                guids = AssetDatabase.FindAssets("t:" + type);
-            }
-            else
-            {
-                guids = AssetDatabase.FindAssets("t:" + type, folders);
-            }
-
-            // Convert asset guids to asset paths
-            List<T> assets = new List<T>();
-            foreach (var guid in guids)
-            {
-                string assetPath = AssetDatabase.GUIDToAssetPath(guid);
-                assets.Add(AssetDatabase.LoadAssetAtPath<T>(assetPath));
-            }
-
-            return assets;
-        }
-#endif
-
-        private void EnsureInitialized()
-        {
-            if (IsInitialized)
-            {
-                return;
-            }
-
-            stringObjMap = new Dictionary<string, ItemData>(references.ToDictionary(item => item.Id));
-            Debug.LogWarning($"{nameof(ItemDataReferences)} was initialized lazily because it wasn't initialized before use!");
-        }
     }
 }
